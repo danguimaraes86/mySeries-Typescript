3f19bb8e613c72b57f02fffb70842fa2191885a2
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-10 15:37:42 -0300

    add: Separator component

---
+++ src/components/Navbar/Separator.tsx
@@ -0,0 +1,7 @@
+import { NextComponentType } from "next"
+
+const Separator: NextComponentType = () => {
+  return <div className='d-none d-md-inline-block flex-fill' />
+}
+
+export default Separator
