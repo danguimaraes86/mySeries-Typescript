59d92e5d2b3350a89e21605aa057a521c44d1a1a
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-13 14:04:50 -0300

    add: API fetch and trending content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,42 @@
-import type { NextPage } from 'next'
-import MySeriesLogo from '../components/Navbar/MySeriesLogo'
-import SearchForm from '../components/Navbar/SearchForm'
-import Separator from '../components/Navbar/Separator'
+import axios from 'axios'
+import type { GetServerSideProps } from 'next'
+import Navbar from '../components/Navbar'
+import TrendinWrapper from '../components/Utils/TrendinWrapper'
+import { handleTrendingSeries } from '../libs/handleTrendingSeries'
 
-const Home: NextPage = () => {
+type Props = {
+  trending: Object[]
+}
 
+function Home({ trending }: Props) {
   return (
-    <nav className='navbar navbar-dark bg-dark rounded-bottom'>
-      <div className='container-fluid'>
-        <MySeriesLogo />
-        <Separator />
-        <SearchForm />
-      </div>
-    </nav>
+    <>
+      <Navbar />
+
+      <TrendinWrapper>
+        {handleTrendingSeries(trending)}
+      </TrendinWrapper>
+    </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { data } = await axios({
+    baseURL: process.env.TMDB_BASEURL,
+    method: 'GET',
+    url: '/trending/tv/week',
+    params: { language: 'pt-BR' },
+    headers: {
+      'Authorization': `Bearer ${process.env.TMDB_APIKEY}`
+    }
+  })
+  const { results: trending } = data
+
+  return {
+    props: {
+      trending,
+    },
+  }
 }
 
 export default Home
