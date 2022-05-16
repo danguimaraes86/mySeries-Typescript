5ac8816cc7f912e9097c35903d726e2f956761ae
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-16 15:44:21 -0300

    refact: add return type to trending fetch

--- src/libs/fetcher.tsx
+++ src/libs/fetcher.tsx
@@ -1,19 +1,20 @@
-import axios from 'axios';
-
-const customAxios = axios.create({
-  baseURL: process.env.TMDB_BASEURL,
-  headers: {
-    'Authorization': `Bearer ${process.env.TMDB_APIKEY}`
-  },
-  params:  { language: 'pt-BR' }
-});
+import { CardDetails } from '../interfaces/CardDetails';
+import customAxios from './customAxios';
 
 export async function fetchTrendingSeries(url: string) {
   const { data } = await customAxios({
     method: 'GET',
     url: url,
   })
-  return data
+  const trending: CardDetails = data.results.map((series: any): CardDetails => {
+    return {
+      id: series.id,
+      name: series.name,
+      poster: series.poster_path,
+      year: series.first_air_date
+    }
+  })
+  return trending
 }
 
 export async function fetchSeriesDetails(url: string, params: object ){
@@ -22,5 +23,7 @@ export async function fetchSeriesDetails(url: string, params: object ){
     url: url,
     params: params
   })
+  console.log(data);
+  
   return data
 }
