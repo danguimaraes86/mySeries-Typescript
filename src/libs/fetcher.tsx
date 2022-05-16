991ecfb8dc27054f6a0363c0a915afa97c445158
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-16 14:17:35 -0300

    add: new fetcher method

--- src/libs/fetcher.tsx
+++ src/libs/fetcher.tsx
@@ -4,14 +4,23 @@ const customAxios = axios.create({
   baseURL: process.env.TMDB_BASEURL,
   headers: {
     'Authorization': `Bearer ${process.env.TMDB_APIKEY}`
-  }
+  },
+  params:  { language: 'pt-BR' }
 });
 
 export async function fetchTrendingSeries(url: string) {
   const { data } = await customAxios({
     method: 'GET',
     url: url,
-    params: { language: 'pt-BR' }
+  })
+  return data
+}
+
+export async function fetchSeriesDetails(url: string, params: object ){
+  const { data } = await customAxios({
+    method: 'GET',
+    url: url,
+    params: params
   })
   return data
 }
