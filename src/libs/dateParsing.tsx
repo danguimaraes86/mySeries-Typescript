65f1a9dec032221260e2d746e4334b7f8c53a067
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-16 14:17:21 -0300

    refact: cleaning code

--- src/libs/dateParsing.tsx
+++ src/libs/dateParsing.tsx
@@ -1,17 +1,9 @@
 import { DateTime } from 'luxon'
 
 export function getYear(date: string) {
-  if(!date){
-    return ''
-  } else {
-    return `(${DateTime.fromISO(date).year})`
-  }
+  return date ? `(${DateTime.fromISO(date).year})` : ''
 }
 
 export function dateLocale(date: string){
-  if(!date){
-    return ''
-  } else {
-    return DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED)
-  }
+  return date ? DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED) : ''
 }
--- src/libs/handleSeriesPoster.tsx
+++ src/libs/handleSeriesPoster.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image'
 
-export function handleSeriesPoster(posterPath: string, name: string){
-  if(!posterPath){
+export function handleSeriesPoster(posterPath: string, name: string) {
+
+  if (!posterPath) {
     return (
       <div className='card-img-top d-flex justify-content-center align-items-center bg-secondary h-100'>
         <i className='bi bi-image text-info ' style={{ fontSize: '6em' }} />
       </div>
     )
-  } else {
-    return (
-      <Image 
-        src={`https://image.tmdb.org/t/p/w500/${posterPath}`}
-        width={500}
-        height={800}
-        className='card-img-top'
-        alt={`poster da série ${name}`}
-      />
-    )
   }
+
+  return (
+    <Image
+      src={`https://image.tmdb.org/t/p/w500/${posterPath}`}
+      width={500}
+      height={800}
+      className='card-img-top'
+      alt={`poster da série ${name}`}
+    />
+  )
+
 }
