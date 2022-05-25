b99bc87bf81a302fdfd41eb7ed1826f02fed923a
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-25 12:17:25 -0300

    fix: remove hardcoded id

--- src/pages/api/series/[id].tsx
+++ src/pages/api/series/[id].tsx
@@ -4,8 +4,8 @@ import { fetcher } from "../../../libs/fetcher"
 export default async function seriesDetails(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
   
-  const series = await fetcher(`/tv/${92749}`, { append_to_response: 'external_ids' })
-  const { results: providers } = await fetcher(`/tv/${92749}/watch/providers`)
+  const series = await fetcher(`/tv/${id}`, { append_to_response: 'external_ids' })
+  const { results: providers } = await fetcher(`/tv/${id}/watch/providers`)
   series['providers'] = providers.hasOwnProperty('BR') ? providers.BR : []
 
   return res.status(200).json(series)
