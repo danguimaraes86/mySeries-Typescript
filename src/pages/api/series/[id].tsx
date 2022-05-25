7c05a1e3a4c66ed31e0e1522f7449188cba39224
Daniel Guimarães <danguimaraes86@gmail.com>
2022-05-25 12:12:30 -0300

    add: API routes

---
+++ src/pages/api/series/[id].tsx
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { fetcher } from "../../../libs/fetcher"
+
+export default async function seriesDetails(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+  
+  const series = await fetcher(`/tv/${92749}`, { append_to_response: 'external_ids' })
+  const { results: providers } = await fetcher(`/tv/${92749}/watch/providers`)
+  series['providers'] = providers.hasOwnProperty('BR') ? providers.BR : []
+
+  return res.status(200).json(series)
+}
