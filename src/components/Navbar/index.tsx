c59369ea7e2c33d13618e934ecf205df87254872
Daniel Guimarães <danguimaraes86@gmail.com>
2022-06-02 11:36:41 -0300

    add: sticky-top to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import SearchForm from './SearchForm'
 
 export default function Navbar() {
   return (
-    <nav className='navbar navbar-dark bg-dark rounded-bottom'>
+    <nav className='navbar navbar-dark bg-dark rounded-bottom sticky-top'>
       <div className='container-fluid'>
         <MySeriesLogo />
         <Separator />
