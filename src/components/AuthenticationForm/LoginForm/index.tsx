476dfb7cd87f07e1b9eb156aafa8509179482c95
Daniel Guimarães <danguimaraes86@gmail.com>
2022-12-01 19:20:24 -0300

    refact: login modal and form

--- src/components/AuthenticationForm/LoginForm/index.tsx
+++ src/components/AuthenticationForm/LoginForm/index.tsx
@@ -6,8 +6,8 @@ export default function LoginForm() {
   const LOGIN_ICON = appIcons.LOGIN_ICON
   const LOGIN_TEXT = 'Login'
 
-  let [userEmail, setUserEmail] = useState('')
-  let [userPassword, setUserPassword] = useState('')
+  let [userEmail, setUserEmail] = useState<string>('')
+  let [userPassword, setUserPassword] = useState<string>('')
 
   function handleLoginSubmit(e: FormEvent) {
     e.preventDefault()
@@ -18,28 +18,26 @@ export default function LoginForm() {
   }
 
   return (
-    <form onSubmit={e => handleLoginSubmit(e)}>
-      <div className='vstack gap-2'>
-        <div className='form-floating'>
-          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com'
-            value={userEmail}
-            onChange={e => setUserEmail(e.target.value)} />
-          <label htmlFor='floatingInput'>Email</label>
-        </div>
-        <div className='form-floating'>
-          <input type='password' className='form-control' id='floatingPassword' placeholder='Password'
-            value={userPassword}
-            onChange={e => setUserPassword(e.target.value)} />
-          <label htmlFor='floatingPassword'>Password</label>
-        </div>
-        <div className='text-center' >
-          <button type='submit' className='btn btn-dark text-info w-100'>
-            <span className='me-1'>
-              {LOGIN_TEXT}
-            </span>
-            <i className={LOGIN_ICON} />
-          </button>
-        </div>
+    <form className='vstack gap-3' onSubmit={e => handleLoginSubmit(e)}>
+      <div className='form-floating'>
+        <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com'
+          value={userEmail}
+          onChange={e => setUserEmail(e.target.value)} />
+        <label htmlFor='floatingInput'>Email</label>
+      </div>
+      <div className='form-floating'>
+        <input type='password' className='form-control' id='floatingPassword' placeholder='Password'
+          value={userPassword}
+          onChange={e => setUserPassword(e.target.value)} />
+        <label htmlFor='floatingPassword'>Password</label>
+      </div>
+      <div className='text-center' >
+        <button type='submit' className='btn btn-dark text-info w-100'>
+          <span className='me-1'>
+            {LOGIN_TEXT}
+          </span>
+          <i className={LOGIN_ICON} />
+        </button>
       </div>
     </form>
   )
--- src/components/AuthenticationModal/LoginModal/index.tsx
+++ src/components/AuthenticationModal/LoginModal/index.tsx
@@ -1,9 +1,13 @@
-import LoginModalBody from "../ModalBody/LoginModalBody";
-import ModalHeader from "../ModalHeader";
+import appIcons from '../../../util/appIcons';
+import LoginModalBody from '../ModalBody/LoginModalBody';
+import ModalHeader from '../ModalHeader';
 
 export default function LoginModal() {
 
   const title = 'Login'
+  const CREATE_ACCOUNT_ICON = appIcons.CREATE_ACCOUNT_ICON
+  const CREATE_ACCOUNT_TEXT = 'Criar nova conta'
+
 
   return (
     <div className='modal fade' id='loginModal' tabIndex={-1}>
@@ -12,6 +16,17 @@ export default function LoginModal() {
           <ModalHeader title={title} />
           <div className='modal-body'>
             <LoginModalBody />
+          </div>
+          <div className='modal-footer justify-content-center'>
+            <button
+              type='submit' className='btn btn-info text-bg-info'
+              data-bs-toggle='modal'
+              data-bs-target='#registrationModal'>
+              <span className='me-2'>
+                {CREATE_ACCOUNT_TEXT}
+              </span>
+              <i className={CREATE_ACCOUNT_ICON} />
+            </button>
           </div>
         </div>
       </div>
